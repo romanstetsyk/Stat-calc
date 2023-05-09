927cad1ce178dd2b7e464c2fc999b2dbdc086ee4
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-05-09 17:05:17 +0100

    add CI limits to the result

--- src/features/OneSampleZSummary/ConfidenceInterval.tsx
+++ src/features/OneSampleZSummary/ConfidenceInterval.tsx
@@ -5,19 +5,25 @@ import DataEditor, {
   GridColumn,
   Item,
 } from "@glideapps/glide-data-grid";
+import quantile from "@stdlib/stats-base-dists-normal-quantile";
+
 import { TForm } from "./types";
 
 type IProps = {
   formSummary: TForm;
 };
 
+// Set column order here
 enum CI {
+  Level = "Level",
   N = "n",
   Xbar = "Sample Mean",
   Stdev = "Std. Dev.",
   Stderr = "Std. Err.",
-  Level = "Level",
   Zcrit = "Z-crit",
+  Me = "M.E.",
+  LL = "L. Limit",
+  UL = "U. Limit",
 }
 
 type ResultRow = {
@@ -27,7 +33,11 @@ type ResultRow = {
 function ConfidenceInterval({ formSummary }: IProps) {
   const { xbar, stdev, n, level } = formSummary;
 
-  const stderr = String(+stdev / Math.sqrt(+n));
+  const stderr = Number(stdev) / Math.sqrt(Number(n));
+  const zcrit = -1 * quantile((1 - Number(level)) / 2, 0, 1);
+  const me = zcrit * stderr;
+  const ll = Number(xbar) - me;
+  const ul = Number(xbar) + me;
 
   const columnHeaders: (GridColumn & { title: CI })[] = useMemo(() => {
     return Object.values(CI).map((e) => ({ title: e, width: 100 }));
@@ -35,12 +45,15 @@ function ConfidenceInterval({ formSummary }: IProps) {
 
   let data: ResultRow[] = [
     {
+      [CI.Level]: level,
       [CI.N]: n,
       [CI.Xbar]: xbar,
       [CI.Stdev]: stdev,
-      [CI.Stderr]: stderr,
-      [CI.Level]: level,
-      [CI.Zcrit]: "42",
+      [CI.Stderr]: String(stderr),
+      [CI.Zcrit]: String(zcrit),
+      [CI.Me]: String(me),
+      [CI.LL]: String(ll),
+      [CI.UL]: String(ul),
     },
   ];
 
