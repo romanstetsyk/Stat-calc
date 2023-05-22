0b10edd2d6dab84f0d271a028173b7d170ad36b0
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-05-22 20:14:27 +0100

    two sample z with data - form

---
+++ src/features/TwoSampleZData/types.ts
@@ -0,0 +1,49 @@
+import { GridColumnName, H0Sign, H1Sign } from "../../Types";
+
+export const SampleStatistics = [
+  "N",
+  "Mean",
+  "Known Stdev",
+  "Std.Err",
+] as const;
+export type SampleStatistics = (typeof SampleStatistics)[number];
+
+export const CIColumns = [
+  "Level",
+  "Z-crit",
+  "Std.Err.",
+  "M.E.",
+  "L.Limit",
+  "U.Limit",
+] as const;
+export type CIColumns = (typeof CIColumns)[number];
+
+export const HTColumns = [
+  "Alpha",
+  "Z-crit",
+  "Std.Err.",
+  "Z-stat",
+  "P-value",
+] as const;
+export type HTColumns = (typeof HTColumns)[number];
+
+export enum PerformType {
+  hypothesisTest = "hypothesisTest",
+  confidenceInterval = "confidenceInterval",
+}
+
+export type TForm = {
+  sample1?: GridColumnName;
+  sample2?: GridColumnName;
+  stdev1?: string;
+  stdev2?: string;
+  perform: PerformType;
+  mu0dir: H0Sign;
+  mu1dir: H1Sign;
+  mu0val: string;
+  mu1val: string;
+  alpha: string;
+  level: string;
+};
+
+export type DisplayOptions = "form" | "result";
