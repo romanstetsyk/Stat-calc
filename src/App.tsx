771663728454f6d071d6adc0c3642f4f65ca9ff0
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-06-05 22:23:05 +0100

    split panes layout component

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,10 @@ import {
   MenuList,
   MenuGroup,
   MenuDivider,
-  Heading,
-  Card,
-  CardHeader,
-  CardBody,
   Flex,
-  IconButton,
-  useBreakpointValue,
 } from "@chakra-ui/react";
-import {
-  ChevronDownIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-} from "@chakra-ui/icons";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 import "allotment/dist/style.css";
-import { Allotment } from "allotment";
 
 import { StatModal as OneSampleZSummaryModal } from "./features/OneSampleZSummary/StatModal";
 import { StatModal as OneSampleZDataModal } from "./features/OneSampleZData/StatModal";
@@ -27,35 +16,11 @@ import { StatModal as FrequencyDistributionModal } from "./features/FrequencyDis
 import { StatModal as GroupNumericDataModal } from "./features/GroupNumericData/StatModal";
 import { StatModal as TwoSampleZSummaryModal } from "./features/TwoSampleZSummary/StatModal";
 import { StatModal as TwoSampleZDataModal } from "./features/TwoSampleZData/StatModal";
-import { Session } from "./layout/Session";
-import { DataGrid } from "./layout/DataGrid";
+
 import { MainHeader } from "./layout/MainHeader";
-import { useEffect, useRef, useState } from "react";
+import { SplitPanes } from "./layout/SplitPanes";
 
 export const App = () => {
-  const [showSessionMobile, setShowSessionMobile] = useState(false);
-  const [showGrid, setShowGrid] = useState(true);
-  const [showSession, setShowSession] = useState(true);
-
-  const [sizes, setSizes] = useState<number[]>([700, 300]);
-
-  console.log("app");
-
-  const smallScreen = useBreakpointValue(
-    { base: true, md: false },
-    { ssr: false }
-  );
-
-  const ref = useRef<{ resize: (args: number[]) => void; reset: () => void }>(
-    null
-  );
-
-  useEffect(() => {
-    if (!smallScreen && ref.current) {
-      ref.current.resize(sizes);
-    }
-  }, [sizes, smallScreen]);
-
   return (
     <Flex direction={"column"} height={"100vh"} overflow={"scroll"}>
       <MainHeader />
@@ -106,83 +71,7 @@ export const App = () => {
         </Menu>
       </Flex>
 
-      {smallScreen && (
-        <IconButton
-          zIndex={1}
-          width={"max-content"}
-          position={"fixed"}
-          top="50%"
-          right="0"
-          onClick={() => {
-            setShowSessionMobile(true);
-          }}
-          aria-label="Add to friends"
-          icon={<ChevronLeftIcon />}
-        />
-      )}
-      {smallScreen && (
-        <IconButton
-          zIndex={1}
-          width={"max-content"}
-          position={"fixed"}
-          top="50%"
-          left="0"
-          onClick={() => {
-            setShowSessionMobile(false);
-          }}
-          aria-label="Add to friends"
-          icon={<ChevronRightIcon />}
-        />
-      )}
-      <Allotment
-        ref={ref}
-        snap
-        defaultSizes={sizes}
-        onDragEnd={setSizes}
-        onVisibleChange={(_index, value) => {
-          console.log("onVisibleChange ran");
-          switch (_index) {
-            case 0:
-              console.log(value);
-              setShowGrid(value);
-              break;
-            case 1:
-              setShowSession(value);
-              break;
-            default:
-              throw new Error("unknown allotment pane");
-          }
-        }}
-      >
-        <Allotment.Pane
-          minSize={100}
-          preferredSize={sizes[0]}
-          visible={smallScreen ? !showSessionMobile : showGrid}
-        >
-          <Card maxW="full" height="100%" m={2}>
-            <CardHeader pb={0}>
-              <Heading size={"md"}>Untitled</Heading>
-            </CardHeader>
-            <CardBody overflow={"scroll"} px={0}>
-              <DataGrid />
-            </CardBody>
-          </Card>
-        </Allotment.Pane>
-        <Allotment.Pane
-          minSize={100}
-          preferredSize={sizes[1]}
-          visible={smallScreen ? showSessionMobile : showSession}
-        >
-          <Card maxW="full" height="100%" m={2}>
-            <CardHeader>
-              <Heading size={"md"}>Session</Heading>
-            </CardHeader>
-            <CardBody overflow={"scroll"}>
-              <Session />
-            </CardBody>
-          </Card>
-        </Allotment.Pane>
-      </Allotment>
+      <SplitPanes />
     </Flex>
   );
 };
---
+++ src/layout/SplitPanes.tsx
@@ -0,0 +1,121 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  IconButton,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { Allotment } from "allotment";
+import { useEffect, useRef, useState } from "react";
+import { DataGrid } from "./DataGrid";
+import { Session } from "./Session";
+
+type AllotmentRef = {
+  resize: (args: number[]) => void;
+  reset: () => void;
+};
+
+export const SplitPanes = () => {
+  const ref = useRef<AllotmentRef>(null);
+
+  const [showSessionMobile, setShowSessionMobile] = useState(false);
+  const [showGrid, setShowGrid] = useState(true);
+  const [showSession, setShowSession] = useState(true);
+
+  const [sizes, setSizes] = useState<number[]>([700, 300]);
+
+  const smallScreen = useBreakpointValue(
+    { base: true, md: false },
+    { ssr: false }
+  );
+
+  useEffect(() => {
+    if (!smallScreen && ref.current) {
+      ref.current.resize(sizes);
+    }
+  }, [sizes, smallScreen]);
+
+  return (
+    <>
+      {smallScreen && (
+        <IconButton
+          zIndex={1}
+          width={"max-content"}
+          position={"fixed"}
+          top="50%"
+          right="0"
+          onClick={() => {
+            setShowSessionMobile(true);
+          }}
+          aria-label="Add to friends"
+          icon={<ChevronLeftIcon />}
+        />
+      )}
+      {smallScreen && (
+        <IconButton
+          zIndex={1}
+          width={"max-content"}
+          position={"fixed"}
+          top="50%"
+          left="0"
+          onClick={() => {
+            setShowSessionMobile(false);
+          }}
+          aria-label="Add to friends"
+          icon={<ChevronRightIcon />}
+        />
+      )}
+      <Allotment
+        ref={ref}
+        snap
+        defaultSizes={sizes}
+        onDragEnd={setSizes}
+        onVisibleChange={(_index, value) => {
+          console.log("onVisibleChange ran");
+          switch (_index) {
+            case 0:
+              console.log(value);
+              setShowGrid(value);
+              break;
+            case 1:
+              setShowSession(value);
+              break;
+            default:
+              throw new Error("unknown allotment pane");
+          }
+        }}
+      >
+        <Allotment.Pane
+          minSize={100}
+          preferredSize={sizes[0]}
+          visible={smallScreen ? !showSessionMobile : showGrid}
+        >
+          <Card maxW="full" height="100%" m={2}>
+            <CardHeader pb={0}>
+              <Heading size={"md"}>Untitled</Heading>
+            </CardHeader>
+            <CardBody overflow={"scroll"} px={0}>
+              <DataGrid />
+            </CardBody>
+          </Card>
+        </Allotment.Pane>
+        <Allotment.Pane
+          minSize={100}
+          preferredSize={sizes[1]}
+          visible={smallScreen ? showSessionMobile : showSession}
+        >
+          <Card maxW="full" height="100%" m={2}>
+            <CardHeader>
+              <Heading size={"md"}>Session</Heading>
+            </CardHeader>
+            <CardBody overflow={"scroll"}>
+              <Session />
+            </CardBody>
+          </Card>
+        </Allotment.Pane>
+      </Allotment>
+    </>
+  );
+};
