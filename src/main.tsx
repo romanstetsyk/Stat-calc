270676270b66dbf5bd1aa2b42ca0fc6092c7fc5a
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-06-18 13:51:44 +0100

    config absolute imports, install react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App.tsx";
 import "./index.css";
 import { SessionProvider } from "./contexts/SessionContext.tsx";
@@ -9,11 +10,13 @@ import { DataColumnsProvider } from "./contexts/DataColumnsContext.tsx";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <DataColumnsProvider>
-        <SessionProvider>
-          <App />
-        </SessionProvider>
-      </DataColumnsProvider>
+      <BrowserRouter>
+        <DataColumnsProvider>
+          <SessionProvider>
+            <App />
+          </SessionProvider>
+        </DataColumnsProvider>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
