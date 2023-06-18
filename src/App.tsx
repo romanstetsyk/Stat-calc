c8d2c260b786f5a579319b790da3230346dbdb79
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-06-18 14:35:14 +0100

    add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import { SharedLayout } from "src/components/SharedLayout";
 
 const Application = lazy(() => import("src/pages/Application"));
+const Home = lazy(() => import("src/pages/Home"));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<div>/</div>} />
+        <Route index element={<Home />} />
         <Route path="/app" element={<Application />} />
         <Route path="/about" element={<div>/about</div>} />
         <Route path="*" element={<div>not found</div>} />
