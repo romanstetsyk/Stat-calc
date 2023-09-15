3d0fbb965dcd48a06de459883efed3bb29409bf0
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-09-15 23:46:55 +0100

    feat: add custom typeguard hasValue

--- backend/src/helpers/type-guards.ts
+++ backend/src/helpers/type-guards.ts
@@ -1,7 +1,12 @@
+import type { ValueOf } from '~/types/types.js';
+
 const hasKeys = (
   obj: unknown,
   ...keys: string[]
 ): obj is Record<(typeof keys)[number], unknown> =>
-  keys.every((key) => typeof obj === 'object' && obj !== null && key in obj);
+  typeof obj === 'object' && obj !== null && keys.every((key) => key in obj);
 
-export { hasKeys };
+const hasValue = (obj: unknown, value: unknown): value is ValueOf<typeof obj> =>
+  typeof obj === 'object' && obj !== null && Object.values(obj).includes(value);
+
+export { hasKeys, hasValue };
