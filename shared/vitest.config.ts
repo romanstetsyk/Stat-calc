92e7388cc3c18e4c57bb0c5ebb0e50de6b15ad01
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-10-12 02:44:39 +0100

    test: shared

---
+++ shared/vitest.config.ts
@@ -0,0 +1,12 @@
+import tsconfigPathsPlugin from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [tsconfigPathsPlugin()],
+  test: {
+    globals: false,
+    clearMocks: true,
+    environment: 'jsdom',
+    exclude: ['build'],
+  },
+});
