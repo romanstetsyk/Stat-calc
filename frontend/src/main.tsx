2e72981c057d010139e557ad1b7ccde4d3874a2f
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-11-02 20:49:44 +0000

    refactor(frontend): move routes to main, delete app.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,22 +4,60 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import type { RouteObject } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import { APP_ROUTES } from '~/common/constants';
+import { SharedLayout } from '~/components/shared-layout';
 import { queryClient } from '~/config/query-client';
 import { SessionProvider } from '~/contexts/session-context';
+import { SignIn } from '~/modules/auth/pages/sign-in';
+import { SignUp } from '~/modules/auth/pages/sign-up';
+import { About } from '~/pages/about';
 
-import { App } from './app';
+const Application = React.lazy(() => import('~/pages/application'));
+const Home = React.lazy(() => import('~/pages/home'));
+
+const routes: RouteObject[] = [
+  {
+    path: APP_ROUTES.HOME,
+    element: <SharedLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: APP_ROUTES.APP,
+        element: <Application />,
+      },
+      {
+        path: APP_ROUTES.ABOUT,
+        element: <About />,
+      },
+      {
+        path: APP_ROUTES.SIGN_UP,
+        element: <SignUp />,
+      },
+      {
+        path: APP_ROUTES.SIGN_IN,
+        element: <SignIn />,
+      },
+      {
+        path: APP_ROUTES.OTHER,
+        element: <div>not found</div>,
+      },
+    ],
+  },
+];
 
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
-        <BrowserRouter>
-          <SessionProvider>
-            <App />
-          </SessionProvider>
-        </BrowserRouter>
+        <SessionProvider>
+          <RouterProvider router={createBrowserRouter(routes)} />
+        </SessionProvider>
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>,
