dccf9e5579639c5b0992d82b57170046260afb17
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-11-02 20:06:32 +0000

    feat(frontend): logo as link

---
+++ frontend/src/common/components/logo/logo.tsx
@@ -0,0 +1,24 @@
+import { Text, useBreakpointValue, useColorModeValue } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+
+import { APP_ROUTES } from '~/common/constants';
+
+const Logo = (): JSX.Element => {
+  return (
+    <Text
+      display='flex'
+      alignItems='center'
+      as={RouterLink}
+      to={APP_ROUTES.HOME}
+      textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+      fontFamily='heading'
+      fontWeight='bold'
+      color={useColorModeValue('gray.800', 'white')}
+      p={2}
+    >
+      NextStat
+    </Text>
+  );
+};
+
+export { Logo };
