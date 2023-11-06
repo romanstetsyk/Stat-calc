a320cf99db3eca895462c915e6fbcd0abd81abf9
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-11-06 22:08:12 +0000

    refactor(frontend): useSignOut hook

--- frontend/src/modules/auth/hooks/use-sign-out.ts
+++ frontend/src/modules/auth/hooks/use-sign-out.ts
@@ -1,11 +1,16 @@
+import type { ErrorCommon } from '@shared/build/esm/index';
+import type { UseQueryResult } from '@tanstack/react-query';
+
 import { useAppQuery, useQueryClient } from '~/common/hooks';
 import { storage } from '~/framework/storage';
 
 import { authApi } from '../auth-api';
 
-const useSignOut = (): ReturnType<typeof useAppQuery<string>> & {
+type UseSignOut = () => UseQueryResult<string, ErrorCommon> & {
   invalidateCurrentUser: () => void;
-} => {
+};
+
+const useSignOut: UseSignOut = () => {
   const queryClient = useQueryClient();
 
   const invalidateCurrentUser = (): void => {
@@ -13,9 +18,13 @@ const useSignOut = (): ReturnType<typeof useAppQuery<string>> & {
     queryClient.removeQueries(['currentUser']);
   };
 
-  const signOutQuery = useAppQuery(['signOut'], authApi.signOut.bind(authApi), {
-    enabled: false,
-  });
+  const signOutQuery = useAppQuery<string, ErrorCommon>(
+    ['signOut'],
+    authApi.signOut.bind(authApi),
+    {
+      enabled: false,
+    },
+  );
 
   return { ...signOutQuery, invalidateCurrentUser };
 };
