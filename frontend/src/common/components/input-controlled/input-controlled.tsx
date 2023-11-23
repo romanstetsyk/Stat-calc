7d8d147c7a5c01f07437d927e1b01c467e0ca28d
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-11-23 15:19:58 +0000

    refactor(frontend): input-controlled component

--- frontend/src/common/components/input-controlled/input-controlled.tsx
+++ frontend/src/common/components/input-controlled/input-controlled.tsx
@@ -1,22 +1,34 @@
 import { Input } from '@chakra-ui/react';
+import type { ComponentProps, HTMLInputTypeAttribute } from 'react';
 import type { FieldValues, UseControllerProps } from 'react-hook-form';
 
 import { useController } from '~/common/hooks';
 
-type InputType = 'text' | 'email' | 'password';
+type InputType = Extract<HTMLInputTypeAttribute, 'text' | 'email' | 'password'>;
 
-type Props<T extends FieldValues> = UseControllerProps<T> & {
-  type: InputType;
-};
+type Props<T extends FieldValues> = UseControllerProps<T> &
+  ComponentProps<typeof Input> & { 'type': InputType };
 
 const InputControlled = <T extends FieldValues>({
-  name,
   type = 'text',
+  name,
   control,
+  defaultValue,
+  rules,
+  shouldUnregister,
+  disabled,
+  ...inputProps
 }: Props<T>): JSX.Element => {
-  const { field } = useController({ name, control });
+  const { field } = useController({
+    name,
+    control,
+    defaultValue,
+    rules,
+    shouldUnregister,
+    disabled,
+  });
 
-  return <Input type={type} {...field} />;
+  return <Input type={type} {...field} {...inputProps} />;
 };
 
 export { InputControlled };
