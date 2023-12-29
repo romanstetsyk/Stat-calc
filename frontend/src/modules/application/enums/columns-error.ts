5d6e69f7a319e1b9b945a884c6fa51b789896602
roman_stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-12-29 12:57:23 +0000

    chore: add columns-error enum

---
+++ frontend/src/modules/application/enums/columns-error.ts
@@ -0,0 +1,6 @@
+const enum ColumnsError {
+  All = 'All columns included in the table have been deleted from the spreadsheet',
+  OneOrMore = 'One or more columns included in the table have been deleted from the spreadsheet',
+}
+
+export { ColumnsError };
--- frontend/src/modules/application/enums/index.ts
+++ frontend/src/modules/application/enums/index.ts
@@ -1,4 +1,5 @@
 export { BinMethod } from './bin-method';
+export { ColumnsError } from './columns-error';
 export { H0Sign } from './h0-sign';
 export { H1Sign } from './h1-sign';
 export { HypothesisType } from './hypothesis-type';
