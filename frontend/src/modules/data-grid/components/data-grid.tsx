9060d48569d7be127239d054e19968a2a350af1c
Roman Stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2024-04-04 21:43:54 +0100

    chore: update eslint

--- frontend/src/modules/data-grid/components/data-grid.tsx
+++ frontend/src/modules/data-grid/components/data-grid.tsx
@@ -1,6 +1,10 @@
 import '@glideapps/glide-data-grid/dist/index.css';
 
-import type { DataEditorRef, GridColumn } from '@glideapps/glide-data-grid';
+import type {
+  DataEditorProps,
+  DataEditorRef,
+  GridColumn,
+} from '@glideapps/glide-data-grid';
 import { DataEditor } from '@glideapps/glide-data-grid';
 import { debounce } from 'lodash-es';
 import { useCallback, useMemo, useRef, useState } from 'react';
@@ -30,25 +34,29 @@ const DataGrid = (): JSX.Element => {
     [],
   );
 
-  const onVisibleRegionChanged = useMemo(
-    () =>
-      debounce(({ x, y, width, height }) => {
-        if (x + width > columnHeaders.length - Config.COLUMNS_LOAD_THRESHOLD) {
-          setColumnHeaders(
-            generateHeaders(
-              columnHeaders.length + Config.COLUMNS_ADDED_PER_SCROLL,
-            ),
-          );
-        }
-        if (y + height > rowCount - Config.ROWS_LOAD_THRESHOLD) {
-          setRowCount(
-            (prev) =>
-              Math.max(prev, rowData.length) + Config.ROWS_ADDED_PER_SCROLL,
-          );
-        }
-      }, Config.DEBOUNCE_TIMEOUT),
-    [columnHeaders.length, rowCount, rowData.length],
-  );
+  const onVisibleRegionChanged: DataEditorProps['onVisibleRegionChanged'] =
+    useMemo(() => {
+      const onVisibleRegionChanged: DataEditorProps['onVisibleRegionChanged'] =
+        ({ x, y, width, height }) => {
+          if (
+            x + width >
+            columnHeaders.length - Config.COLUMNS_LOAD_THRESHOLD
+          ) {
+            setColumnHeaders(
+              generateHeaders(
+                columnHeaders.length + Config.COLUMNS_ADDED_PER_SCROLL,
+              ),
+            );
+          }
+          if (y + height > rowCount - Config.ROWS_LOAD_THRESHOLD) {
+            setRowCount(
+              (prev) =>
+                Math.max(prev, rowData.length) + Config.ROWS_ADDED_PER_SCROLL,
+            );
+          }
+        };
+      return debounce(onVisibleRegionChanged, Config.DEBOUNCE_TIMEOUT);
+    }, [columnHeaders.length, rowCount, rowData.length]);
 
   return (
     <DataEditor
